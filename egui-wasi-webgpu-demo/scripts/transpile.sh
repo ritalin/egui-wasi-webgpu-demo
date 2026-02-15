d59be2c1c9bca227b55c002b1c15ca3bc6f4f42c
Ritalin <kazu.ktz.tamura@gmail.com>
2026-02-16 01:00:39 +0900

    Add release build in the script.

--- egui-wasi-webgpu-demo/scripts/transpile.sh
+++ egui-wasi-webgpu-demo/scripts/transpile.sh
@@ -2,11 +2,22 @@
 
 set -e -o pipefail
 
-cargo build --target wasm32-wasip2 --manifest-path ./pkg/wasi-renderer/Cargo.toml
+PROFILE_FLAG=""
+PROFILE_DIR="debug"
+OPTIMIZE=""
+
+if [ "$1" == "release" ]; then
+  PROFILE_FLAG="--release"
+  PROFILE_DIR="release"
+  OPTIMIZE="--optimize"
+fi
+
+cargo build --target wasm32-wasip2 --manifest-path ./pkg/wasi-renderer/Cargo.toml $PROFILE_FLAG
 
 pnpm exec jco transpile \
   --name egui-renderer \
   --out-dir pkg/_transpiled \
   --instantiation async \
   --no-nodejs-compat \
-  pkg/wasi-renderer/target/wasm32-wasip2/debug/wasi_renderer.wasm
+  $OPTIMIZE \
+  pkg/wasi-renderer/target/wasm32-wasip2/$PROFILE_DIR/wasi_renderer.wasm
