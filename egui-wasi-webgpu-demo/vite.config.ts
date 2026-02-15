a7d51a3d60af261d2065edce3eac158d7ea7b07a
Ritalin <kazu.ktz.tamura@gmail.com>
2026-02-16 01:56:08 +0900

    Add publish configuration

--- egui-wasi-webgpu-demo/vite.config.ts
+++ egui-wasi-webgpu-demo/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import * as path from "node:path";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/egui-wasi-webgpu-demo/" : "/",
   resolve: {
     alias: {
       "egui-renderer": path.resolve(
